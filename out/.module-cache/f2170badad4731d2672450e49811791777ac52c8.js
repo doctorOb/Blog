077fddaf26d7dbd78ef21b3e2fb06ee0a1ed06ab
Dan Robertson <drobertson@kayak.com>
2014-01-26 10:31:21 -0500

    first commit

---
+++ out/.module-cache/f2170badad4731d2672450e49811791777ac52c8.js
@@ -0,0 +1,31 @@
+/** @jsx React.DOM */
+
+console.log('called');
+var week1 = React.createClass({displayName: 'week1',
+	render: function() {
+		return (
+			React.DOM.div( {className:"journal week"}, 
+			React.DOM.time( {className:"date"}, "Wednesday, January 21"),
+			React.DOM.p(null, 
+				"Over the next seven week's, I'll be diving head first into a craft often overlooked by some, music production. I feel as though typical listeners attribute musical success to strictly compositional qualities, like a good hook, or catchy lyrics. But the success of a good recording hinges on the skills of it's producer. While I can manage to contrast some differences between two arbitrary recordings, I'd be crazy to claim any real understanding of what goes on behind the scenes - in the studio. Over the next 7 weeks, I'll be taking a deep dive into production techniques, as I apply them in my own works."
+			),
+
+			React.DOM.h2( {className:"ui header"}, React.DOM.i( {className:"coffee icon"}),"Influences"),
+				React.DOM.p(null, 
+					"Back in High School, I was a metal head. No getting around it. I played in a few bands that tried to emulate the all to common metalcore sound, which was good fun but didn't pan out over time. In college, I continued to create music, but never towards a greater purpose then personal enjoyment. What I've amassed is dozens of hours of short, amateur iPhone recordings of motives and ideas that I would soon forget about. They became more of an exercise in creativity and musical technique. I believe that cataloging your creations is invaluable, and found recently that some musicians, such as ",React.DOM.a( {target:"_blank", href:"http://www.nerdist.com/2013/08/nerdist-podcast-rogue-wave/"}, "Zach Rogue"),", agree and do the same."
+				),
+
+				React.DOM.p(null, 
+					"I started playing guitar when I was in the 7th grade, after being inspired by the music game", React.DOM.span( {className:"italic"}, "Guitar Hero"),". When I was 17, I bought a decent bass so I could complete the lineup in a folk rock band that I'd formed with a few friends. This lead to my first recording experience, at a audio production school in Boston. I would later buy my first Acoustic guitar, which profoundly warped my play style and listening preferences. I never enjoyed playing the over popularized and de facto style of Acoustic common in any ",
+						React.DOM.a( {target:"_blank", href:"http://youtu.be/nhivdSMdMxc?t=24s", title:"4 chords, 35 songs"}, "four chord song."),
+						" I got into math rock, listening to bands like ",
+						React.DOM.a( {target:"_blank", href:"http://topshelfrecords.bandcamp.com/album/songs-ideas-we-forgot"}, "Toe"),',', React.DOM.a( {target:"_blank", href:"http://americanfootball.bandcamp.com/album/american-football"}, "American Football"),', and ',React.DOM.a( {target:"_blank", href:"http://thistownneedsguns.bandcamp.com/"}, "This Town Needs Guns"),"."
+				),
+
+			React.DOM.h2( {className:"ui header"}, React.DOM.i( {className:"coffee icon"}),"The EP")
+
+				
+		)	
+		);
+	}
+});
---
+++ scripts/blog.js
@@ -0,0 +1,41 @@
+/** @jsx React.DOM */
+
+function loadiFrames(week) {
+	week.find('.embed.unloaded').each(function(){
+		var $this = $(this);
+		var iFrame = $this.data('iframe');
+		$this.html(iFrame);
+		$this.removeClass('unloaded');
+	});
+}
+
+(function($){
+	$(document).ready(function(){
+
+		React.renderComponent(<Blog/>,document.getElementById('1'));
+		$('.week').waypoint(function(direction){
+
+			loadiFrames($(this));
+			var week = parseInt($(this).attr('id'),10),
+				offset = direction === 'up' ? week - 1 : week,
+				next;
+			$('.active').removeClass('active');
+			next = $('#week-' + offset);
+			if (next) {
+				next.addClass('active');
+			}
+		},{offset: "25%"});
+
+		$('.persistent-nav .item').on('click',function(e){
+			e.preventDefault();
+			var week = $(this).attr('id').match(/\d+/)[0];
+
+			console.log($('#' + week)[0].offsetTop);
+			$('body').stop(true,true);
+			$('body').animate({
+				scrollTop: $('#' + week)[0].offsetTop
+			},500);
+		});
+	});
+
+})(jQuery);
